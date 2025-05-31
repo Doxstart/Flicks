818cb2594972f659a0c64a3dd85585d713f38a65
Doxstart <127298109+Doxstart@users.noreply.github.com>
2025-05-31 15:29:12 +0200

    Add clarification comment

--- flicks.rb
+++ flicks.rb
@@ -2,5 +2,5 @@ title = "Ghostbusters"
 rank = 9
 puts "#{title} has a rank of #{rank}"
 puts "Mikey's favorite movie is #{title}.\nHe gives it a #{rank}!"
-#Solo se puede realizar interpolación con "" 
+#Solo se puede realizar interpolación y new line (\n) con "" 
 #Pero también se puede definir un string dentro de una variable con ''
