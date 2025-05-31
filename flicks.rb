814051cf4ae353fcdad6991b98805eee5dd3278a
Doxstart <127298109+Doxstart@users.noreply.github.com>
2025-05-31 16:06:27 +0200

    Add more info in comment
    Defining self object and how it gets referenced with or without a method.

--- flicks.rb
+++ flicks.rb
@@ -4,3 +4,6 @@ puts "#{title} has a rank of #{rank}"
 puts "Mikey's favorite movie is #{title}.\nHe gives it a #{rank}!"
 #Solo se puede realizar interpolación y new line (\n) con "" 
 #Pero también se puede definir un string dentro de una variable con ''
+#self sirve para referenciar un objeto cualquiera 
+#self es implicito cuando se usa otro metodo, por ejemplo puts
+#Queda implicito que puts es self.puts, pero no es necesario escribirlo
