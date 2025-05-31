f7f326768d22aa668e8334fab18b27d1e2df7064
Doxstart <127298109+Doxstart@users.noreply.github.com>
2025-05-31 15:01:33 +0200

    Finish 3rd lesson
    Completed third lesson (ABCs and 123s) of Ruby Programming by Pragmatic Studio.

--- flicks.rb
+++ flicks.rb
@@ -1,5 +1,6 @@
-puts "Ruby is friendly!"
-
-phrase = "Ruby is fun!"
-
-puts phrase
+title = "Ghostbusters"
+rank = 9
+puts "#{title} has a rank of #{rank}"
+puts "Mikey's favorite movie is #{title}.\nHe gives it a #{rank}!"
+#Solo se puede realizar interpolación con "" 
+#Pero también se puede definir un string dentro de una variable con ''
